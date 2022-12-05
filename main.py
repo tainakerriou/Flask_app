495a7ccda95e63f406dfbca66bdfca135aef381f
tainakerriou <taina.kerriou@epfedu.fr>
2022-12-05 16:34:31 +0100

    commit

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 
 def hello_world():
-prefix_google = """
-<!-- Google tag (gtag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-251027646-1"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-251027646-1');
-</script>
-"""
-return prefix_google + "Hello World"
+	prefix_google = """
+	<!-- Google tag (gtag.js) -->
+	<script async
+	src="https://www.googletagmanager.com/gtag/js?id=UA-251027646-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-251027646-1');
+	</script>
+	"""
+	return prefix_google + "Hello World"
