e4993a98788f6fc939a730e839b680cd612e6fa3
tainakerriou <taina.kerriou@epfedu.fr>
2022-12-05 16:10:35 +0100

    commit

--- main.py
+++ main.py
@@ -4,5 +4,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def app(event):
-    return "Hello, world!"
+
+def hello_world():
+prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-251027646-1');
+</script>
+"""
+return prefix_google + "Hello World"
