ca5cd6a6a1460ebabc9a82e1cfd1daeb576262c0
tainakerriou <taina.kerriou@epfedu.fr>
2023-01-03 12:00:19 +0100

    TP2 fini

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
-from flask import Flask
-import logging
+import requests
+from flask import Flask, render_template
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+#from dotenv import load_dotenv
+#import os
+
 
 app = Flask(__name__)
+#load_dotenv(".env")
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'datasource-373523-383f8cf8a396.json' #str(os.getenv("file_json"))
+VIEW_ID = '281180695' #str(os.getenv("view_id"))
+
 
 @app.route('/', methods=["GET"])
 
@@ -57,6 +67,60 @@ def printlog():
 
 	</script>
 	"""
+
 	print('log in Deta')
-	logging.warning('Watch out bébé!')
-	return prefix_google 
+
+	return prefix_google
+
+
+def initialize_analyticsreporting():
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILE_LOCATION, SCOPES)
+  analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+  return analytics
+
+
+def get_report(analytics):
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:pageviews'}],
+          'dimensions': []
+        }]
+      }
+  ).execute()
+
+
+def get_visitors(response):
+  visitors = 0 # in case there are no analytics available yet
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+    for row in report.get('data', {}).get('rows', []):
+      dateRangeValues = row.get('metrics', [])
+
+      for i, values in enumerate(dateRangeValues):
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          visitors = value
+
+  return str(visitors)
+
+@app.route('/visitors')
+
+def visitors():
+  analytics = initialize_analyticsreporting()
+  response = get_report(analytics)
+  visitors = get_visitors(response)
+
+  return render_template('visitors.html', visitors=str(visitors))
+
+if __name__ == '__main__':
+	app.run()
+        
+
+	
